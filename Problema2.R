23006dba5031102f632517126d48033c6590d09d
paula.bosch <paula.bosch@auleseebe.upc.edu>
2025-02-13 13:43:20 +0100

    exercicis

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,36 @@
 #Codigo para problema 2
+#taula de freq
+data<- mtcars
+data
+x<-data$cyl
+ni<-table(x)
+ni
+fi<-ni/length(x)
+fi
+Ni<-cumsum(ni)
+Ni
+Fi<-cumsum(fi)
+Fi
+#per escriure en forma de matriu
+cbind(ni, fi, Ni, Fi)
+
+#continuos, tallar x en 10 intervals regulars
+x<-data$mpg
+bins<- cut(x, 10)
+bins
+ni<-table(bins)
+fi<-ni/length(bins)
+Ni<-cumsum(ni)
+Fi<-cumsum(Fi)
+cbind(ni, fi, Ni, Fi)
+
+#histograma
+hist(data$mpg, breaks=10)
+
+#discretes:barplot es fa sobre ni, 
+ni<-table(data$cyl)
+barplot(ni)
+
+#pie: discretes
+fi<- ni/length(data$cyl)
+pie(fi)
--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,15 @@
-#Codigo para problema 3
+#CODI EX 3,  sample(0:10, 10, replace=TRUE)
+#seleccionar exemple: x[c(1,3,20)], eliminar ex: x[-c(1,3,20)]
+xi<-sum(8,6,0,1,7,5,8,1,3,4)
+xi
+yi<-sum(3,7,8)
+yi
+xi-yi
+
+###
+x<-c(8,6,0,1,7,5,8,1,3,4)
+sum(x)
+y<-x[-c(3,7,8)]
+sum(exp(x))-sum(exp(y))
+y[5]
+y[2]
